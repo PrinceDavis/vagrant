1bd4c0cab3b5792ef34360aa93e48ff929088c2d
Ossaija ThankGod <codebugsolved@gmail.com>
2017-07-04 12:16:38 +0100

    updated nodejs.sh

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -4,5 +4,5 @@ curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 
 sudo apt-get install -y nodejs
-sudo apt-get install -y make
-sudo apt-get install build-essential
+sudo apt-get install -y gcc-c++ openssl-devel make
+sudo apt-get install -y build-essential
