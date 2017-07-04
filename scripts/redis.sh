0dcae2fcf3f2c9c65b01e07d756b660a48ffdb18
Ossaija ThankGod <codebugsolved@gmail.com>
2017-07-04 08:43:00 +0100

    add script for setting up mean stack

---
+++ scripts/redis.sh
@@ -0,0 +1,10 @@
+cd ~
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+sudo make install
+sudo mkdir /etc/redis
+sudo cp /tmp/redis-stable/redis.conf /etc/redis
+sudo touch /etc/systemd/system/redis.service
