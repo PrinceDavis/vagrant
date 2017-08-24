224730bec524a449d5fca76a079a921df1673fde
Ossaija ThankGod <codebugsolved@gmail.com>
2017-08-24 15:30:07 +0100

    deleted unworking scripts and added script for setting up nde evironment on ubuntu6.04

--- scripts/nodejs.sh
+++
@@ -1,8 +0,0 @@
-cd ~
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-
-sudo bash nodesource_setup.sh
-
-sudo apt-get install -y nodejs
-sudo apt-get install -y gcc-c++ openssl-devel make
-sudo apt-get install -y build-essential
---
+++ scripts/ubuntu-for-node-app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#update server
+apt-get update -y
+
+#install node 8.x
+cd ~
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs
+sudo apt-get install build-essential
