07e31e412bb07abacaa4f1df806da137ed4d51a0
Ossaija ThankGod <codebugsolved@gmail.com>
2017-08-24 15:56:47 +0100

    update node script to verify node and npm installation

--- scripts/ubuntu-for-node-app.sh
+++ scripts/ubuntu-for-node-app.sh
@@ -4,7 +4,9 @@ apt-get update -y
 
 #install node 8.x
 cd ~
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install nodejs
 sudo apt-get install build-essential
+sudo node -v
+sudo npm -v
