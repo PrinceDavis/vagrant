01a3d478161c29a9002704d7d355df10de980ea5
Ossaija ThankGod <codebugsolved@gmail.com>
2017-08-25 03:29:08 +0100

    added script to configure, startup and verify mongodb installation

--- scripts/ubuntu-for-node-app.sh
+++ scripts/ubuntu-for-node-app.sh
@@ -6,6 +6,7 @@ apt-get update -y
 sudo apt-get install build-essential tcl
 
 #install node 8.x
+echo "Installing node 8 and npm"
 cd ~
 curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
@@ -15,8 +16,18 @@ echo "Verifying node and npm installation"
 sudo node -v
 sudo npm -v
 
-#innstall mongodb 
+#innstall mongodb
+echo "Installing mongodb"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+#configuring mongodb
+echo "Configuring mongodb"
+sudo cp /home/vagrant/code/application/scripts/.mongodb /etc/systemd/system/mongodb.service
+
+#startup mongodb
+echo "Startup and verify mongodb service"
+sudo systemctl start mongodb
+sudo systemctl status mongodb
